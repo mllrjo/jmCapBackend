0cd3a2fd0ae3b15342c98b69e91e26da214f66c1
mllrjo <159646777+mllrjo@users.noreply.github.com>
2025-02-21 14:51:27 -0500

    db.mjs 2

--- server.mjs
+++ server.mjs
@@ -1,12 +1,15 @@
-// build off of Dylan's basic backend
+// build off of Dylan's basic backend starter
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.mjs';
+
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3003;
 
-app.use(express.json());
+app.use(express);
+connectDB();
 
 // Listener
 app.listen(PORT, () => {
